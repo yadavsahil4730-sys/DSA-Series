1097f3834cbbc6e0a689a4e463563dcc3f94a6d1
yadavsahil-sys <yadavsahil4730@gmail.com>
2026-09-17 17:41:11 +0530

    setColor

---
+++ 01-Arrays/setColor.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sortColors(vector<int>& nums) {
+    int low = 0;
+    int mid = 0;
+    int high = nums.size() - 1;
+
+    while (mid <= high) {
+
+        if (nums[mid] == 0) {
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        }
+
+        else if (nums[mid] == 1) {
+            mid++;
+        }
+
+        else {
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+}
+
+int main() {
+
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+
+    sortColors(nums);
+
+    // Array print karne ke liye
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
